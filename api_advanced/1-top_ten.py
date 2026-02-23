958f26602bd0139eeafca2f1c2724f074e89b6bd
Lennie02 <o.ishimwe1@alustudent.com>
2026-02-23 02:47:07 +0000

    Fixing.

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 """Module to query Reddit API for top 10 hot posts."""
-import json
-import urllib.request
+import requests
 
 
 def top_ten(subreddit):
     """Print titles of first 10 hot posts for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    req = urllib.request.Request(
-        url,
-        headers={"User-Agent": "MyBot/1.0"}
-    )
-    try:
-        with urllib.request.urlopen(req) as response:
-            data = json.loads(response.read().decode("utf-8"))
-            posts = data.get("data", {}).get("children", [])
-            for post in posts:
-                print(post.get("data", {}).get("title"))
-    except Exception:
+    headers = {"User-Agent": "linux:myredditapp:v1.0 (by /u/myusername)"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         print("None")
+        return
+    data = response.json()
+    posts = data.get("data", {}).get("children", [])
+    for post in posts:
+        print(post.get("data", {}).get("title"))
